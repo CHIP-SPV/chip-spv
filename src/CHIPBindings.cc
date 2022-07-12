cc1538473dde100787acc8b37625d31dc873c3c5
Michal Babej <michal.babej@parmance.com>
2022-07-12 15:24:37 +0300

    update SPIR-V support to version 1.2
    .... using spirv.hpp11 header.
    
    Version 1.2 is the highest supported by Intel GPU OpenCL.

--- src/CHIPBindings.cc
+++ src/CHIPBindings.cc
@@ -25,6 +25,7 @@
 #include "hip/hip_fatbin.h"
 #include "hip/hip_runtime_api.h"
 #include "hip_conversions.hh"
+#include "common.hh"
 #include "macros.hh"
 
 #define SPIR_BUNDLE_ID "hip-spir64-unknown-unknown"
